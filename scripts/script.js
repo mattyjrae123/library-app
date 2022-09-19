230a1a5a630048175dbd42a4a37bc219e93cda66
Matthew Rae <mattyjrae123@hotmail.com>
2022-09-19 13:47:17 +1200

    Add js doc to Book constructor

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,13 @@
 
 let library = [];
 
+/**
+ * Constructor for book objects that stores details of the book
+ * @param {string} title Title of book
+ * @param {string} author Author of book
+ * @param {number} pages Number of pages in book
+ * @param {boolean} read If book has been read
+ */
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
